3848c0841f8aa1ce32f07543be5472771a0d8eef
Chu <zh.c.l.z.95@gmail.com>
2023-04-30 10:53:24 +0100

    base class

---
+++ src/agg_base.py
@@ -0,0 +1,204 @@
+import numpy as np
+# from scipy import stats
+# from typing import Tuple
+
+
+class aggregate_distribution:
+    '''
+    Base class for aggregate distributions.
+
+    We define the frequency, severity elements. Possible losses are also to be included as a grid depending on the underlying algorithm.
+
+    Both distributions to be dicts taking the form
+
+    {
+        dist: any of scipy.stats,
+        properties: statistical parameters required (in dict form)
+    }
+
+    See scipy docs for the required properties
+
+    Parameters
+    ----------
+    frequency_distribution: as described above
+    severity_distribution: as described above
+
+    Properties
+    ----------
+    losses: vector of possible losses (scaled, if needed)
+    agg_pdf: accessible discretized aggregate loss pdf
+
+    Callables
+    ---------
+    Some basic stats can be called for the underlying frequency and severity distributions. These are mostly calls to their scipy.stats functions. Refer to scipy manual for more details.
+
+    Call compile_aggregate_distribution() to generate the aggregate distribution.
+    '''
+
+    def __init__(
+            self,
+            frequency_distribution: dict,
+            severity_distribution: dict,
+            discretization_step: float = 0.01,
+            grid: float = 1048576
+            ):
+        '''
+        Initialize the frequency and severity distributions.
+        '''
+        self.frequency = frequency_distribution
+        self.severity = severity_distribution
+
+        self.h = discretization_step
+        self.M = grid
+
+        self.severity_dpdf = 0
+        self.agg_pdf = 0
+        self.losses = 0
+
+    def get_frequency_mean(self) -> float:
+        '''
+        Returns the mean of the chosen frequency distribution
+        '''
+        return self.frequency['dist'].mean(*self.frequency['properties'])
+
+    def get_severity_mean(self) -> float:
+        '''
+        Returns the mean of the chosen severity distribution
+        '''
+        return self.severity['dist'].mean(*self.severity['properties'])
+
+    def get_frequency_variance(self) -> float:
+        '''
+        Returns the variance of the chosen frequency distribution
+        '''
+        return self.frequency['dist'].variance(*self.frequency['properties'])
+
+    def get_severity_variance(self) -> float:
+        '''
+        Returns the variance of the chosen severity distribution
+        '''
+        return self.severity['dist'].variance(*self.severity['properties'])
+
+    def agg_mean(self,
+                 theoretical: bool = True
+                 ):
+        '''
+        Returns the mean of the aggregate distribution. If `theoretical` is set to true, we return
+
+        E(severity)*E(frequency)
+
+        Otherwise return the approximation
+        '''
+        if theoretical:
+            return self.get_severity_mean() * self.get_frequency_mean()
+        else:
+            return self.agg_pdf * self.losses
+
+    def agg_variance(self,
+                     theoretical: bool = True
+                     ):
+        '''
+        Returns the variance of the aggregate distribution. If `theoretical` is set to true, we return
+
+        E(frequency)*V(severity) + V(frequency)*V(frequency)^2
+
+        Otherwise return the approximation
+        '''
+        if theoretical:
+            return self.get_frequency_mean()*self.get_severity_variance() + self.get_frequency_variance()*(self.get_severity_mean())**2
+        else:
+            return self.agg_pdf * (self.losses**2) - self.agg_mean(theoretical)**2
+
+    def discretize_pdf(self,
+                       X: np.ndarray | None = None
+                       ):
+        '''
+        Discretize a provided severity distribution according to:
+
+        pdf(X) = dist.cdf(X+h/2, dist_params) - dist.cdf(X-h/2, dist_params)
+
+        Parameters
+        ----------
+        X: np.ndarray [optional],
+            Input for the pdf. If None is passed, then we use self.losses and update internal discrete severity pdf
+
+        Returns
+        -------
+        pdf: np.ndarray | None
+            Discretized pdf corresponding to the input X, if provided, otherwise None
+        '''
+        dist = self.severity['dist']
+        dist_params = self.severity['properties']
+        h = self.h
+
+        dpdf = dist.cdf(X+h/2, *dist_params) - dist.cdf(X-h/2, *dist_params)
+        if X is None:
+            assert (dpdf.sum() >= 1 - h) & (dpdf.sum() <= 1 + h), 'Error: invalid discrete severity dist: does not total 1'
+            self.severity_dpdf = dpdf
+        else:
+            return dpdf
+
+    def setup_layer(self,
+                    excess: float,
+                    limit: float,
+                    inplace: bool = True):
+        '''
+        Set up a suitable insurance structure with each-and-avery excess and limits.
+
+        Layer designed by adjusting probability of incurring losses after excess and limit.
+
+        Parameters
+        ----------
+        excess: float,
+            Excess retained (by insured)
+        limit: float
+            Limit, above which losses are ceded
+        inplace: bool [Optional]
+            If set, we overwrite the current discretized severity PDF, otherwise, return the discretized PDF as a vector, which can be inserted into another object
+
+        Returns
+        -------
+        dpdf: np.ndarray | None
+            Discretized severity PDF after layer modification (only when inplace is not set to True)
+        '''
+
+        # Losses: we expect the severity to already be discretized at this stage so lazy it
+        if self.severity_dpdf == 0:
+            self.discretize_pdf()
+
+        # Discretize the limit and excess points
+        lh = int(limit / self.h)
+        xh = int(excess / self.h)
+
+        p_xs_survival = 1 - np.sum(self.severity_dpdf[:xh])
+
+        # Treatment of excess
+        dpdf = np.zeros(self.grid)
+        dpdf[:lh] = self.severity_dpdf[range(xh, lh)] / p_xs_survival
+
+        # Treatment of limit
+        dpdf[lh] = 1 - dpdf.sum()
+
+        if inplace:
+            self.severity_dpdf = dpdf
+
+            # Also override the frequency
+            self.thin_frequency(p_xs_survival)
+        else:
+            return dpdf
+
+    def compile_aggregate_distribution(self):
+        '''
+        Classes to inherit and implement this function!
+        '''
+        raise NotImplementedError
+
+    def thin_frequency(n: float):
+        '''
+        Redefine the frequency distribution as a result of thinning.
+
+        Theorem: E(Thin frequency) = n*E(frequency)
+
+        The variance and definition will depend on the underyling frequency itself so this will need custom implementation!
+        '''
+        raise NotImplementedError
