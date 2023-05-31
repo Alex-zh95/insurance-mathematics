f1c395817ed620f13734bb090940ca4f6d495cc8
Alex <zh.c.l.z.95@gmail.com>
2023-05-31 16:39:31 +0100

    Method of moments for negative binomial

--- pkg/lib/nb.py
+++ pkg/lib/nb.py
@@ -9,6 +9,27 @@ We can then solve the likelihood function for the shape parameter.
 
 import numpy as np
 from scipy.special import digamma
+from typing import Tuple
+
+
+def nb_method_of_moments(data: np.ndarray) -> Tuple[float, float]:
+    '''
+    Use method of moments estimator to attain parameters of negative binomial distribution.
+
+    Parameters
+    ----------
+    data: np.ndarray
+        Data to use
+
+    Returns
+    -------
+    r, k: float, float
+        Shape and scale of the negative binomial distribution (see scipy.stats.nbinom)
+    '''
+    mean, var = data.mean(), data.var()
+    r = mean**2/(var - mean)
+    k = mean/var
+    return (r, k)
 
 
 def nb_likelihood(r: float, data: np.ndarray) -> float:
