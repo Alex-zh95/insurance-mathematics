ea0f18b9989eea8a54596bc87a54be69952bd5f2
Chu <zh.c.l.z.95@gmail.com>
2023-08-18 10:38:38 +0100

    Added semi-automated procedure for frequency distribution parameterization

--- ins_mat/lib/nb.py
+++ ins_mat/lib/nb.py
@@ -8,17 +8,19 @@ We can then solve the likelihood function for the shape parameter.
 '''
 
 import numpy as np
+import pandas as pd
 from scipy.special import digamma
+from scipy.optimize import newton
 from typing import Tuple
 
 
-def nb_method_of_moments(data: np.ndarray) -> Tuple[float, float]:
+def nb_method_of_moments(data: np.ndarray | pd.DataFrame | pd.Series) -> Tuple[float, float]:
     '''
     Use method of moments estimator to attain parameters of negative binomial distribution.
 
     Parameters
     ----------
-    data: np.ndarray
+    data: np.ndarray | pd.DataFrame | pd.Series
         Data to use
 
     Returns
@@ -32,7 +34,7 @@ def nb_method_of_moments(data: np.ndarray) -> Tuple[float, float]:
     return (r, k)
 
 
-def nb_likelihood(r: float, data: np.ndarray) -> float:
+def nb_likelihood(r: float, data: np.ndarray | pd.DataFrame | pd.Series) -> float:
     '''
     Likelihood function based on initial known shape parameter r (as per scipy implementation) and provided data.
 
@@ -40,7 +42,7 @@ def nb_likelihood(r: float, data: np.ndarray) -> float:
     ----------
     r: float
         Shape parameter for negative binomial distribution (see scipy.stats.nbinom)
-    data: np.ndarray
+    data: np.ndarray | pd.DataFrame | pd.Series
         Data vector to use
 
     Returns
@@ -55,7 +57,7 @@ def nb_likelihood(r: float, data: np.ndarray) -> float:
     return sum_digamma + log
 
 
-def nb_mle_get_prob(r: float, data: np.ndarray) -> float:
+def nb_mle_get_prob(r: float, data: np.ndarray | pd.DataFrame | pd.Series) -> float:
     '''
     Returns the scale parameter given the shape parameter, r.
 
@@ -63,7 +65,7 @@ def nb_mle_get_prob(r: float, data: np.ndarray) -> float:
     ----------
     r: float
         Shape parameter for negative binomial distribution (see scipy.stats.nbinom)
-    data: np.ndarray
+    data: np.ndarray | pd.DataFrame | pd.Series
         Data vector to use
 
     Returns
@@ -73,3 +75,47 @@ def nb_mle_get_prob(r: float, data: np.ndarray) -> float:
     '''
     N = data.shape[0]
     return N*r/(N*r + np.sum(data))
+
+
+def get_frequency_measures(data: pd.DataFrame | pd.Series | np.ndarray,
+                           excl_nil: bool = True,
+                           prompt: str = "sample") -> Tuple[float, float] | float:
+    '''
+    Attempt to fit frequency distributions to provided data. The negative binomial distribution is prioritized and will be returned if there are no errors in convergence or no invalid parameters are returned. Otherwise, we will return a Poisson distribution parameter.
+
+    Distributional parameters all determined by maximum likelihood.
+
+    Parameters
+    ----------
+    data: pd.DataFrame | pd.Series | np.ndarray
+        Data to be fitted
+    excl_nil: bool = True
+        Exclude nil data points (i.e. exclude when data == 0) in fit
+    prompt: str = "sample"
+        A label for the dataset (optional), to aid with diagnostic reports.
+
+    Returns
+    -------
+    estimators: Tuple[float, float] | float
+        The negative binomial shape and scale paramters if succesful on fitting a negative binomial distribution. Otherwise, return the Poisson mu parameter.
+    '''
+
+    _data = data[data > 0] if excl_nil else data
+
+    N_mean = _data.mean()
+
+    # NB fits by MLE are iterative, initialize with initial guess via Method of Moments
+    nb_init = nb_method_of_moments(_data)
+
+    try:
+        r = newton(nb_likelihood, nb_init[0], args=(_data))
+        k = nb_mle_get_prob(r, _data)
+    except RuntimeError:  # Failure to converge results in this error
+        print('MLE convergence failed for {prompt}. Reverting to Poisson frequency.')
+        return N_mean
+
+    if r < 0:
+        print('Invalid negative binomial shape parameter ({k:,.5f}) for {prompt}. Reverting to Poisson frequency.')
+        return N_mean
+
+    return (r, k)
