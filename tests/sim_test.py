31b112eb4d57b36a42e143812993891a0028d4a1
Alex <zh.c.l.z.95@gmail.com>
2025-07-03 22:11:15 +0100

    Added tests for simulations

--- tests/sim_test.py
+++ tests/sim_test.py
@@ -4,30 +4,31 @@ import sys
 # If not locally installed (or at least in editable mode), append path the base dir and src dir for module imports
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
-from insurance_mathematics.agg_dist.fft_nb import Agg_NbFft
-
+from insurance_mathematics.agg_dist.agg_sim import AggSim
 from scipy import stats
 import numpy as np
 
 import unittest
 
 
-def percentage_error(observed: float, expected: float) -> float:
-    return np.abs(observed - expected) / expected
+def in_tol(observed: float, expected: float) -> bool:
+    error = np.abs(observed - expected) / expected
+    return error < 0.1
 
 
-class Test_FFT_NB(unittest.TestCase):
-    frequency: dict
+class Test_Sim(unittest.TestCase):
+    freq_poisson: dict
     severity: dict
     layer: list
-    agg_lim: float
+
+    # NOTE: Turn off parallel proceessing here for tests - joblib parallel may not allow isolation
+    # which unittest requires.
 
     @classmethod
     def setUpClass(cls) -> None:
-        # Set up parameters for the test class
-        cls.frequency = {
-            'dist': stats.nbinom,
-            'properties': [3.0, 0.85]
+        cls.freq_poisson = {
+            'dist': stats.poisson,
+            'properties': [0.5]
         }
 
         cls.severity = {
@@ -36,46 +37,49 @@ class Test_FFT_NB(unittest.TestCase):
         }
 
         cls.layer = [3.0, 10.0]
-        cls.agg_lim = 8.5
+
         return super().setUpClass()
 
     def test_ground_up(self):
         '''Testing consistency of ground up FFT calculations vs exact.'''
-        n, p = self.frequency['properties']
-        mdl = Agg_NbFft(n=n, p=p, severity_distribution=self.severity)
+        mdl = AggSim(frequency_distribution=self.freq_poisson,
+                     severity_distribution=self.severity,
+                     parallel=False)
         mdl.compile_aggregate_distribution()
 
-        fft_mean = mdl.mean()
+        sim_mean = mdl.mean()
         exact_mean = mdl.mean(True)
 
-        self.assertAlmostEqual(percentage_error(fft_mean, exact_mean), 0.0, 2)
+        self.assertTrue(in_tol(sim_mean, exact_mean))
 
     def test_layer(self):
         '''Testing consistency of calculations in layer between fft and exact.'''
-        n, p = self.frequency['properties']
-        mdl = Agg_NbFft(n=n, p=p, severity_distribution=self.severity)
+        mdl = AggSim(frequency_distribution=self.freq_poisson,
+                     severity_distribution=self.severity,
+                     parallel=False)
 
         mdl.setup_layer(*self.layer)
         mdl.compile_aggregate_distribution()
 
-        fft_mean = mdl.mean()
+        sim_mean = mdl.mean()
         exact_mean = mdl.mean(True)
 
-        self.assertAlmostEqual(percentage_error(fft_mean, exact_mean), 0.0, 2)
+        self.assertTrue(in_tol(sim_mean, exact_mean))
 
     def test_variance(self):
         '''Test consistency of calcs for variance between FFT and exact.'''
-        n, p = self.frequency['properties']
-        mdl = Agg_NbFft(n=n, p=p, severity_distribution=self.severity)
+        mdl = AggSim(frequency_distribution=self.freq_poisson,
+                     severity_distribution=self.severity,
+                     parallel=False)
 
         mdl.setup_layer(*self.layer)
         mdl.compile_aggregate_distribution()
 
-        fft_var = mdl.var()
+        sim_var = mdl.var()
         exact_var = mdl.var(True)
 
-        self.assertAlmostEqual(percentage_error(fft_var, exact_var), 0.0, 2)
-    
+        self.assertTrue(in_tol(sim_var, exact_var))
+
 
 if __name__ == '__main__':
     unittest.main()
